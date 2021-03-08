8cb9f35781b4cf7e11dbdf3bda584c07d7eb5c22
saedjabali <saad.jabali@yahoo.com>
2021-03-08 13:40:24 +0200

    styling and adding files

---
+++ app.js
@@ -0,0 +1,36 @@
+alert('Hello');
+
+alert('This is a test page for Js');
+
+var age=prompt('How old are you');
+
+var alloawnce = function (){
+
+
+
+if (age >= 18){
+    
+     
+    
+        for(var i=0; i<=3; i++)
+        {
+            document.write('You can enroll' + '<br>');
+        } 
+        
+        }
+        
+  
+else 
+{
+    for(var i=0; i<=3; i++)
+        {
+            document.write('You cannot enroll' + '<br>');
+        } 
+        
+        }
+        
+
+}
+    
+alloawnce(); 
+    
